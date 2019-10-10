dcdf502e8e95257cb4dff2c8659ae0c011c2e945
Quentin Metivier <quentin.metivier@consertotech.pro>
2019-10-11 00:24:47 +0200

    add OperationRepo

--- src/Repository/OperationRepository.java
+++ src/Repository/OperationRepository.java
@@ -1,34 +1,125 @@
 package Repository;
 
+import BO.Compte;
 import BO.Operation;
+import Connection.PersistanteManager;
 
 import java.io.IOException;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
 public class OperationRepository implements IRepository<Operation>, AutoCloseable {
 
-    @Override
-    public ArrayList<Operation> getAll() {
-        return null;
-    }
-
-    public Operation getOneById() {
-        return null;
-    }
+    private static final String INSERT_QUERY = "INSERT INTO operation (compteDebId, compteBenefId, montant) VALUES(?,?,?)";
+    private static final String UPDATE_QUERY = "UPDATE operation SET compteDebId = ?, compteBenefId = ?, montant = ? WHERE id = ?";
+    private static final String REMOVE_QUERY = "DELETE FROM operation WHERE id = ?";
+    private static final String FIND_BY_ID_QUERY = "SELECT * FROM operation WHERE id = ?";
+    private static final String FIND_ALL_QUERY = "SELECT * FROM operation";
 
     @Override
-    public void Add(Operation _object) {
+    public ArrayList<Operation> getAll() throws SQLException, IOException, ClassNotFoundException {
+        ArrayList<Operation> listOperation = new ArrayList<>();
+        Connection connection = PersistanteManager.getConnection();
+        if ( connection != null ) {
+            try ( PreparedStatement ps = connection.prepareStatement( FIND_ALL_QUERY ) ) {
+                try ( ResultSet rs = ps.executeQuery() ) {
+                    if ( rs.next() ) {
+                        Operation operation = new Operation();
+                        operation.setId( rs.getInt( "id" ) );
+                        operation.setCompteDeb(
+                                (new CompteRepository().getOneById( rs.getInt("compteDebId") ) )
+                        );
+                        operation.setCompteBenef(
+                                (new CompteRepository().getOneById( rs.getInt("compteBenefId") ) )
+                        );
+                        operation.setMontant( rs.getDouble( "montant" ) );
+                        listOperation.add(operation);
+                    }
+                }
+            }
+        }
+        return listOperation;
+    }
+
+    public Operation getOneById(Integer id) throws SQLException, IOException, ClassNotFoundException {
+
+        Operation operation = null;
+        Connection connection = PersistanteManager.getConnection();
+        if ( connection != null ) {
+            try ( PreparedStatement ps = connection.prepareStatement( FIND_BY_ID_QUERY ) ) {
+                ps.setInt( 1, id );
+                try ( ResultSet rs = ps.executeQuery() ) {
+                    if ( rs.next() ) {
+                        operation = new Operation();
+                        operation.setId( rs.getInt( "id" ) );
+                        operation.setCompteDeb(
+                                (new CompteRepository().getOneById( rs.getInt("compteDebId") ))
+                        );
+                        operation.setCompteBenef(
+                                (new CompteRepository().getOneById( rs.getInt("compteBenefId") ) )
+                        );
+                        operation.setMontant( rs.getDouble("montant") );
+                    }
+                }
+            }
+        }
+        return operation;
 
     }
 
     @Override
-    public void Remove(Operation _object) {
+    public void Add(Operation _object) throws SQLException, IOException, ClassNotFoundException {
+
+        Connection connection = PersistanteManager.getConnection();
+        if ( connection != null ) {
+            try ( PreparedStatement ps = connection.prepareStatement( INSERT_QUERY ) ) {
+                ps.setInt( 1, _object.getCompteDeb().getId() );
+                ps.setInt( 2, _object.getCompteBenef().getId() );
+                ps.setDouble( 3, _object.getMontant() );
+                try ( ResultSet rs = ps.executeQuery() ) {
+                    if ( rs.next() ) {
+                        _object.setId( rs.getInt( 1 ) );
+                    }
+                }
+            }
+        }
+
+    }
+
+    @Override
+    public void Remove(Operation _object) throws SQLException, IOException, ClassNotFoundException {
+
+        Connection connection = PersistanteManager.getConnection();
+        if ( connection != null ) {
+            try ( PreparedStatement ps = connection.prepareStatement( REMOVE_QUERY ) ) {
+                ps.setInt( 1, _object.getId());
+
+            }
+        }
 
     }
 
     @Override
     public void Update(Operation _object) throws SQLException, IOException, ClassNotFoundException {
+
+        Connection connection = PersistanteManager.getConnection();
+        if ( connection != null ) {
+            try ( PreparedStatement ps = connection.prepareStatement( UPDATE_QUERY ) ) {
+                ps.setInt( 1, _object.getCompteDeb().getId() );
+                ps.setInt( 2, _object.getCompteBenef().getId() );
+                ps.setDouble(3, _object.getMontant());
+                ps.setInt( 4, _object.getId());
+                try ( ResultSet rs = ps.executeQuery() ) {
+                    if ( rs.next() ) {
+                        _object.setId( rs.getInt( 1 ) );
+                    }
+                }
+            }
+        }
+
 
     }
 
